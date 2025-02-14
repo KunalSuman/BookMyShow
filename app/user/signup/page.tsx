4040420ae4cdae148337a84428fd337c11b6266d
Kunal <kunal23295@iiitd.ac.in>
2025-02-14 18:24:38 +0530

    user Frontend

---
+++ app/user/signup/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useRouter } from "next/router";
+import { useState } from "react";
+export default function() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [phone, setPhone] = useState("");
+    const [balace , setBalance] = useState(0);
+    const navigate = useRouter();
+    return(
+        <div className="bg-slate-700 h-screen w-screen flex justify-center items-center">
+            <div className="min-h-screen w-full flex justify-center items-center p-8 py-8">
+            <div className=" rounded-md flex flex-col bg-white h-[50vh] items-center justify-between text-bolded text-black w-1/3 p-4" >
+                <div className="text-2xl font-bold">
+                    SignUp
+                </div>
+                <input type = "text" placeholder="email" className="border-2 rounded-md h-1/6 w-1/2 p-4" onChange={(e)=>{setEmail(e.target.value)}}></input>
+                <input type = "name" placeholder="name" className="border-2 rounded-md h-1/6 w-1/2 p-4" onChange={(e)=>{setName(e.target.value)}}></input>
+                <input type = "phone" placeholder="phone" className="border-2 rounded-md h-1/6 w-1/2 p-4" onChange={(e)=>{setPhone(e.target.value)}}></input>
+                <input type = "password" placeholder="password" className="border-2 rounded-md w-1/2 h-1/6 p-4" onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}></input>
+                <button className="bg-blue-500 w-1/3 rounded-md h-1/6" onClick={()=>{
+                    navigate.push('/home')
+                }}>Sign in</button>
+            </div>
+            </div>
+
+        </div>
+    )
+}
